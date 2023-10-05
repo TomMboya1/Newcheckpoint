784d94082757cc5b22639b1855875013bd1f1d57
tom mboya <tommboya044@gmail.com>
2023-10-05 11:13:24 +0300

    1st commit

---
+++ src/main/java/checkPoint/CheckPoint2Application.java
@@ -0,0 +1,13 @@
+package checkPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckPoint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckPoint2Application.class, args);
+	}
+
+}
---
+++ src/test/java/checkPoint/CheckPoint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package checkPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckPoint2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
